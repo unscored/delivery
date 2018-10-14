4c70b98aab7e6a95fd87abca2560cb7ca05ecdd1
Vitaliy Marushko <vi.marushko@gmail.com>
2018-10-14 20:02:00 +0300

    Added example method for getting list of orders

--- server/api/products.js
+++ server/api/products.js
@@ -1,13 +1,39 @@
 'use strict';
 
 const models = require('../models');
+const errors = require('../utils/errors');
 
 module.exports = {
-  getList: async (params, callback) => {
+  getList2: async (params, callback) => {
     try {
-      let recordsArray = [];
+      const query = `
+        select
+          o.id
+        , c.name
+        , c.phone
+        , o.status
+        , SUM(v.price) as total
+        from ordersProducts
+          inner join orders o on ordersProducts.orderId = o.id
+          inner join clients c on o.clientId = c.id
+          inner join \`values\` v on ordersProducts.valueId = v.id
+        where
+          c.phone = :phone
+        group by
+          o.id
+          , c.name
+          , c.phone
+          , o.status;
+      `;
 
-      callback(null, recordsArray);
+      const result = await models.sequelize.query(query, {
+        replacements: {
+          'phone': '+38 (000) 123-44-44'
+        },
+        type: models.sequelize.QueryTypes.SELECT
+      });
+
+      callback(null, result);
     } catch (ex) {
       console.log('api/products.js | get | exception:', ex);
       callback(ex);
