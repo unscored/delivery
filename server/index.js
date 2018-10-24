ffe34f5aed5e5cf9ddbe5195adc8a78cd13de92f
unscored <crewcoast@gmail.com>
2018-10-24 16:37:20 +0300

    public path added

--- server/index.js
+++ server/index.js
@@ -9,15 +9,17 @@ const direct = require('./utils/direct');
 const server = require('./utils/server');
 const app = express();
 
+const PUBLIC_PATH = path.join(__dirname, '..', 'public');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(cors());
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(PUBLIC_PATH));
 
 app.get(['/', '/cart', '/contacts'], function(req, resp) {
-  resp.sendFile("/public/index.html");
+  resp.sendFile(PUBLIC_PATH + '/index.html');
 });
 
 direct.init(app);
