2dff8e57dec79a6a895b583ace46443386b70b4a
unscored <crewcoast@gmail.com>
2018-10-24 16:57:10 +0300

    update site title remove node js favicon

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const cors = require('cors');
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(cors());
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use(express.static(PUBLIC_PATH));
 
 app.get(['/', '/cart', '/contacts'], function(req, resp) {
