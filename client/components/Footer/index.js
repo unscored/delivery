6b8a3d0e7942f5e13e1afa40a42487ca0b6f76cd
unscored <crewcoast@gmail.com>
2019-03-24 23:57:44 +0200

    add insta link in footer

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { FaInstagram } from 'react-icons/fa';
 
+import { cssMQ } from '../../utils';
 import css from './Footer.scss';
 
 const Footer = () => {
@@ -8,7 +10,10 @@ const Footer = () => {
       <div className={css.top}>
         <div className={css.contacts}>
           <p>+38 063 37 32 915</p>
-          <p className={css.email}>pizza-belvedere@gmail.com</p>
+          <a href='https://www.instagram.com/cafe_belvedere/' target="_blank" className={css.insta}>
+            <FaInstagram size={cssMQ.isMobile() ? 22 : 38} color={'#000'}/>
+            <span>cafe_belvedere</span>
+          </a>
         </div>
       </div>
       <div className={css.bottom}>
