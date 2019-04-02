cbc826492956c0398a56795ce607ee321b47c78c
unscored <crewcoast@gmail.com>
2019-04-02 19:25:51 +0300

    add redicrect to https if user goes to http

--- server/index.js
+++ server/index.js
@@ -11,6 +11,18 @@ const { spaRoutes } = require('./config');
 
 const PUBLIC_PATH = path.join(__dirname, '..', 'public');
 
+app.enable('trust proxy');
+
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
